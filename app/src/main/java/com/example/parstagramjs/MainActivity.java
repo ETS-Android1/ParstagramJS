6e2e106ac17f194d7ecf163d054613d1ee4298fe
Joseph Santantonio <js958@njit.edu>
2021-10-14 21:50:12 -0400

    Pulling posts from database

--- app/src/main/java/com/example/parstagramjs/MainActivity.java
+++ app/src/main/java/com/example/parstagramjs/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.parstagramjs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnTakePicture;
+    private Button btnSubmit;
+    private ImageView ivPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        ivPicture = findViewById(R.id.ivPicture);
+
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if ( e != null ) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                // Retrieving posts was successful
+                for ( Post post : posts ) {
+                    Log.i(TAG, "Post: " + post.getDescription());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/parstagramjs/ParseApplication.java
+++ app/src/main/java/com/example/parstagramjs/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.parstagramjs;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("dLK7j6Z1r6P4Nyt0W7X2QFaD4BcSp34Bnxjqha98")
