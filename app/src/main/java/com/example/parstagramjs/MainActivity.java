c259658962e07be7c6dcf927fe7481ba6faf815f
Joseph Santantonio <js958@njit.edu>
2021-10-14 21:52:58 -0400

    update query posts

--- app/src/main/java/com/example/parstagramjs/MainActivity.java
+++ app/src/main/java/com/example/parstagramjs/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit = findViewById(R.id.btnSubmit);
         ivPicture = findViewById(R.id.ivPicture);
 
+        queryPosts();
+
     }
 
     private void queryPosts() {
